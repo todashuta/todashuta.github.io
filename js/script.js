22a8a1d16c5c5a329914a91ca87e9236b8833dc0
todashuta <toda.shuta@gmail.com>
2015-02-10 13:44:11 +0900

    Redirect to https

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 (function() {
+	if (location.protocol === 'http:') {
+		location.protocol = 'https:';
+	}
+
 	window.addEventListener('DOMContentLoaded', function() {
 		console.log('Hello World!');
 	}, false);
