58bed48964fd2c40d9220a627494f020b07eaa57
todashuta <toda.shuta@gmail.com>
2016-06-11 20:51:45 +0900

    Add joke script

--- js/script.js
+++ js/script.js
@@ -5,6 +5,24 @@ window.addEventListener('DOMContentLoaded', function() {
   console.log('Hello World!');
 }, false);
 
+// For Google Chrome Android...
+(function() {
+  'use strict';
+  var idx;
+  var colors = ['red', 'green', 'blue'];
+  var elemThemeColor = document.querySelector('meta[name=theme-color]');
+
+  // Initialize
+  elemThemeColor.content = colors[0];
+
+  var update = function() {
+    elemThemeColor.content =
+      colors[(colors.indexOf(elemThemeColor.content)+1)%colors.length];
+  }
+
+  setInterval(update, 1000);
+})();
+
 })();
 
 /* vim: set et ts=2 sts=2 sw=2 : */
