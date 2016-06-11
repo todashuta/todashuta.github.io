864586863b17c25259bd501cfcdb4fb4e91961d8
todashuta <toda.shuta@gmail.com>
2016-06-11 15:59:56 +0900

    Remove unnecessary code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 (function() {
-	if (location.protocol === 'http:') {
-		location.protocol = 'https:';
-	}
-
 	window.addEventListener('DOMContentLoaded', function() {
 		console.log('Hello World!');
 	}, false);
